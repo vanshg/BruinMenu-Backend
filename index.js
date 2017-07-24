515d64a3d65759f894560b8737315c7543f1006b
Sahil Gandhi <sahilmgandhi@gmail.com>
2017-07-24 00:04:50 -0700

    Add hours information to front page

--- index.js
+++ index.js
@@ -45,10 +45,17 @@ app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(express.static('website'))
+app.use(express.static(__dirname + '/images'));
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 
 /* Parameters:
